dd044a812f61a60b5e7606e7b6fa5b7588fe29d1
sethoak <sethoak@gmail.com>
2019-08-29 13:09:21 -0500

    created a branch and saving

---
+++ example/js/example.js
@@ -0,0 +1,8 @@
+/*
+    Basic object representing my dog Angus.
+    - The object itself is labeled with the variable of `dog`
+    - The integer of 1 is labeled with the key of `age`
+*/
+const angus = {
+  age: 1
+};
